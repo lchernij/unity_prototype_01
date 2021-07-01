63b02f09fd049b66a7e73f394f7608a775209731
Leandro Chernij <lchernij@gmail.com>
2021-07-01 09:48:04 -0300

    First commit

---
+++ Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    private float turnSpeed = 200.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, Time.deltaTime * turnSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private float turnSpeed = 21.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    public Camera mainCamera;
+    public Camera seatCamera;
+    public KeyCode switchKey = KeyCode.V;
+
+    // Multiplayer
+    public string inputID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            seatCamera.enabled = !seatCamera.enabled;
+        }
+    }
+}
